144704034807d80b3fd45338f0f6e96b6226d071
Marceau CRANDAL KABBANI <m.zeldacyrano@gmail.com>
2025-12-20 11:44:24 +0100

    Ajoute la demande de playlist, d'album et d'artiste et augment la fiabilitée du script

--- RV.py
+++ RV.py
@@ -4,49 +4,82 @@ import webbrowser
 import time
 import pyautogui
 import sys
-def recherche_titre(nom):
+import keyboard
+from nava import play
+def recherche_titre(type, nom):
     try:
-        url = f"https://api.deezer.com/search/track?q={nom}"
+        url = f"https://api.deezer.com/search/{type}?q={nom}"
         response = requests.get(url)
         data = response.json()
-        return data["data"][0]
+        if data == response.json():
+            print("Recherche effectuée avec succès.")
+            return data["data"][0]
+        else:
+            print("Aucun résultat trouvé.")
+            sys.exit()
     except Exception:
         print("Erreur lors de la recherche du titre.")
-        sys.exit(1)
 
-def recherch_bouton(image_path): 
-    try:
-        bouton = pyautogui.locateOnScreen(image_path, confidence=0.8)
-        pyautogui.click(bouton.left + bouton.width // 2, bouton.top + bouton.height // 2)
-        return bouton
-    except pyautogui.ImageNotFoundException:
-        essais =+ 1
-        if essais > 10:
-            time.sleep(1)
-            print("Bouton non trouvé.")
-            sys.exit(1)
+def recherch_bouton(image_path):
+    start_time = time.time()
+    while time.time() - start_time < 5:
+        try:
+            bouton = pyautogui.locateOnScreen(image_path, confidence=0.8)
+            if bouton:
+                pyautogui.click(bouton.left + bouton.width // 2,bouton.top + bouton.height // 2)
+                print("Bouton cliqué.")
+                return bouton
+        except pyautogui.ImageNotFoundException:
+            plein_ecran()
         time.sleep(0.1)
-        recherch_bouton(image_path)
-    
+    print("Bouton non trouvé après 5 secondes.")
+
+def plein_ecran():
+    keyboard.press_and_release('f11')
 if __name__ == '__main__':
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        play("start_enr.mp3")
         print("Parlez maintenant...")
         audio = r.listen(source)
     try:
         text = r.recognize_google(audio, language="fr-FR")
-        print(f"Vous avez dit : {text}")
-        if text == "Flow":
+        type = text.split(" ", 1)[0]
+        titre = text.split(" ", 1)[1:]
+        print(f"Vous avez dit : {type} {titre}")
+        if type == "Flow":
             print("Fonction en dévellopement")
-        else:
-            url = recherche_titre(text)
-            print(f"Titre : {url['title']}, Artiste : {url['artist']['name']}")
-            webbrowser.open(f"deezer://{url['link']}?")
+        elif type == "titre" or type == "track" or type == "chanson":
+            response = recherche_titre("track", titre)
+            print(f"Titre : {response['title']}, Artiste : {response['artist']['name']}")
+            webbrowser.open(f"deezer://{response['link']}?")
             time.sleep(1)
-            pyautogui.hotkey("win", "up")
-            recherch_bouton("play.png")
+            recherch_bouton("images/play.png")
+        elif type == "album":
+            response = recherche_titre("album", titre)
+            print(f"Album : {response['title']}, Artiste : {response['artist']['name']}")
+            webbrowser.open(f"deezer://{response['link']}?")
+            time.sleep(1)
+            recherch_bouton("images/playlist.png")
+        elif type == "artiste":
+            response = recherche_titre("artist", titre)
+            print(f"Artiste : {response['name']}")
+            webbrowser.open(f"deezer://{response['link']}?")
+            time.sleep(1)
+            recherch_bouton("images/artiste.png")
+        elif type == "playlist":
+            response = recherche_titre("playlist", titre)
+            print(f"Playlist : {response['title']}")
+            webbrowser.open(f"deezer://{response['link']}?")
+            time.sleep(1)
+            recherch_bouton("images/playlist.png")
+        else:
+            print("Type non reconnu. Veuillez dire 'Flow', 'Titre', 'Album', 'Artiste' ou 'Playlist' puis le titre.")
+            time.sleep(5)
+            sys.exit()
+        
                 
     except sr.UnknownValueError:
         print("Impossible de comprendre.")
     except sr.RequestError:
-        print("Erreur de connexion à l'API Speech Recognition.")
+        print("Erreur de connexion à l'API de reconnaisance vocale.")
