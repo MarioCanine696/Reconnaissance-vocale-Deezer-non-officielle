9db7ad7e26b33f70e91f698e2987e16cf5b8633d
Marceau CRANDAL KABBANI <m.zeldacyrano@gmail.com>
2025-12-21 10:36:47 +0100

    Maximise la fenetre a la place du pleine ecran et clique qu'une fois sur le bouton play

--- RV.py
+++ RV.py
@@ -4,8 +4,8 @@ import webbrowser
 import time
 import pyautogui
 import sys
-import keyboard
 from nava import play
+import pygetwindow as gw
 def recherche_titre(type, nom):
     try:
         url = f"https://api.deezer.com/search/{type}?q={nom}"
@@ -25,12 +25,13 @@ def recherche_titre(type, nom):
 
 def recherch_bouton(image_path):
     start_time = time.time()
-    while time.time() - start_time < 5:
+    while time.time() - start_time < 10:
         try:
             bouton = pyautogui.locateOnScreen(image_path, confidence=0.8)
             if bouton:
                 pyautogui.click(bouton.left + bouton.width // 2,bouton.top + bouton.height // 2)
                 print("Bouton cliqué.")
+                break
         except pyautogui.ImageNotFoundException:
             plein_ecran()
         time.sleep(0.1)
@@ -38,7 +39,9 @@ def recherch_bouton(image_path):
     time.sleep(2)
 
 def plein_ecran():
-    keyboard.press_and_release('f11')
+    fenetre = gw.getWindowsWithTitle("Deezer")[0]
+    if not fenetre.isMaximized:
+        fenetre.maximize()
 
 def reconnaissance_vocale():
     r = sr.Recognizer()
@@ -71,36 +74,33 @@ if __name__ == '__main__':
         response = recherche_titre("track", titre)
         print(f"Titre : {response['title']}, Artiste : {response['artist']['name']}")
         webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(1)
+        time.sleep(2)
         recherch_bouton("images/play.png")
     elif type == "album" or type == "record" or type == "disque":
         response = recherche_titre("album", titre)
         print(f"Album : {response['title']}, Artiste : {response['artist']['name']}")
         webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(1)
+        time.sleep(2)
         recherch_bouton("images/playlist.png")
     elif type == "artiste" or type == "artist" or type == "auteur" or type == "chanteur":
         response = recherche_titre("artist", titre)
         print(f"Artiste : {response['name']}")
         webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(1)
+        time.sleep(2)
         recherch_bouton("images/artiste.png")
     elif type == "playlist" or type == "liste" or type == "playliste":
         response = recherche_titre("playlist", titre)
         print(f"Playlist : {response['title']}")
         webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(1)
+        time.sleep(2)
         recherch_bouton("images/playlist.png")
     else:
         print("Type non reconnu. Veuillez dire 'Flow', 'Titre', 'Album', 'Artiste' ou 'Playlist' puis le titre.")
-        time.sleep(1)
+        time.sleep(0.5)
         print("Recherche par défaut du titre...")
-        time.sleep(1)
+        time.sleep(0.5)
         response = recherche_titre("track", titre)
         print(f"Titre : {response['title']}, Artiste : {response['artist']['name']}")
         webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(1)
+        time.sleep(2)
         recherch_bouton("images/play.png")
-        print("Type non reconnu. Veuillez dire 'Flow', 'Titre', 'Album', 'Artiste' ou 'Playlist' puis le titre.")
-        time.sleep(5)
-        sys.exit()
