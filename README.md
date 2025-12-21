eb4b649bfa3f31de0f188e7f55469a88aa365b59
Marceau CRANDAL KABBANI <m.zeldacyrano@gmail.com>
2025-12-21 18:52:29 +0100

    Ajoute la demande de flow et corrige des bug

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Reconnaisance vocale Deezer - Projet personnel de MarioCanine696
+# Reconnaisance vocale Deezer - Projet personnel de Marceau CRANDAL KABBANI
+Cette application n'est pas officielle et est réservée à un usage personnel
 L'application pour bureau deezer est recommandée
 Pour faire fonctionner ce script : "pip install -r requirements.txt"
 Inclu un fichier ahk pour autokey (recommandé)
--- RV.py
+++ RV.py
@@ -6,6 +6,9 @@ import pyautogui
 import sys
 from nava import play
 import pygetwindow as gw
+
+version = "beta 1.0.0"
+
 def recherche_titre(type, nom):
     try:
         url = f"https://api.deezer.com/search/{type}?q={nom}"
@@ -27,16 +30,31 @@ def recherch_bouton(image_path):
     start_time = time.time()
     while time.time() - start_time < 10:
         try:
+            plein_ecran()
             bouton = pyautogui.locateOnScreen(image_path, confidence=0.8)
             if bouton:
                 pyautogui.click(bouton.left + bouton.width // 2,bouton.top + bouton.height // 2)
                 print("Bouton cliqué.")
                 break
-        except pyautogui.ImageNotFoundException:
-            plein_ecran()
+        except Exception:
+            pass
         time.sleep(0.1)
-    print("Bouton non trouvé après 5 secondes.")
+    print("Bouton non trouvé après 10 secondes.")
     time.sleep(2)
+
+def recherche_flow():
+    start_time = time.time()
+    while time.time() - start_time < 10:
+        try:
+            plein_ecran()
+            bouton = pyautogui.locateOnScreen("images/flow.png", confidence=0.8)
+            if bouton:
+                pyautogui.click(bouton.left + bouton.width // 2,bouton.top + bouton.height // 2)
+                print("Flow cliqué.")
+                break
+        except Exception:
+           pyautogui.scroll(-500) 
+        time.sleep(0.1)
 
 def plein_ecran():
     fenetre = gw.getWindowsWithTitle("Deezer")[0]
@@ -59,48 +77,63 @@ def reconnaissance_vocale():
         print("Impossible de comprendre.")
         time.sleep(2)
     except sr.RequestError:
-        print("Erreur de connexion à l'API de reconnaisance vocale google.")
+        print("Erreur de connexion à l'API de reconnaisance vocale.")
         time.sleep(2)
         sys.exit()
 
-if __name__ == '__main__':
-    text = reconnaissance_vocale()
-    type = text.split(" ", 1)[0]
-    titre = text.split(" ", 1)[1:]
-    if type == "Flow":
-        print("Fonction en dévellopement")
+def analyse_son(son):
+    type = son.split(" ", 1)[0]
+    titre = son.split(" ", 1)[1:]
+    if type in ["flow", "flo", "Flow", "Flo"]:
+        print("Recherche du Flow...")
         time.sleep(2)
+        return None
     elif type == "titre" or type == "track" or type == "chanson":
         response = recherche_titre("track", titre)
         print(f"Titre : {response['title']}, Artiste : {response['artist']['name']}")
-        webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(2)
-        recherch_bouton("images/play.png")
+        return response
     elif type == "album" or type == "record" or type == "disque":
         response = recherche_titre("album", titre)
-        print(f"Album : {response['title']}, Artiste : {response['artist']['name']}")
-        webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(2)
-        recherch_bouton("images/playlist.png")
+        return response
     elif type == "artiste" or type == "artist" or type == "auteur" or type == "chanteur":
         response = recherche_titre("artist", titre)
         print(f"Artiste : {response['name']}")
-        webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(2)
-        recherch_bouton("images/artiste.png")
+        return response
     elif type == "playlist" or type == "liste" or type == "playliste":
         response = recherche_titre("playlist", titre)
         print(f"Playlist : {response['title']}")
-        webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(2)
-        recherch_bouton("images/playlist.png")
+        return response
     else:
         print("Type non reconnu. Veuillez dire 'Flow', 'Titre', 'Album', 'Artiste' ou 'Playlist' puis le titre.")
-        time.sleep(0.5)
+        time.sleep(0.2)
         print("Recherche par défaut du titre...")
-        time.sleep(0.5)
+        time.sleep(0.2)
         response = recherche_titre("track", titre)
         print(f"Titre : {response['title']}, Artiste : {response['artist']['name']}")
+        return response
+
+if __name__ == '__main__':
+    print("###########################################################")
+    print(f'#     Reconnaissance vocale Deezer version {version}     #')
+    print("###########################################################")
+    print("Cette application n'est pas officielle et est réservée à un usage personnel")
+    print("Si un problème survient, merci de contacter l'auteur.")
+    son = reconnaissance_vocale()
+    response = analyse_son(son)
+    if response is None:
+        webbrowser.open("deezer://https://www.deezer.com/fr/")
+        recherche_flow()
+    else:
+        type = son.split(" ", 1)[0]
         webbrowser.open(f"deezer://{response['link']}?")
-        time.sleep(2)
-        recherch_bouton("images/play.png")
+        if type in ["album", "record", "disque", "playlist", "liste", "playliste"]:
+            time.sleep(2)
+            recherch_bouton("images/playlist.png")
+        if type in ["artiste", "artist", "auteur", "chanteur"]:
+            time.sleep(2)
+            recherch_bouton("images/artiste.png")
+        if type in ["flow", "flo", "Flow", "Flo"]:
+            pass
+        else:
+            time.sleep(2)
+            recherch_bouton("images/play.png")
